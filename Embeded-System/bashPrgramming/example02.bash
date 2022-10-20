762ccadabde021e76587fa1152cdde9083d7d0aa
Frank Linux <yongfrank@outlook.com>
2022-10-20 15:35:40 +0800

    add bash files

--- Embeded-System/bashPrgramming/example02.bash
+++ Embeded-System/bashPrgramming/example02.bash
@@ -1,3 +1,11 @@
+###
+ # @Author: Frank Linux yongfrank@outlook.com
+ # @Date: 2022-10-20 15:17:16
+ # @LastEditors: Frank Linux
+ # @LastEditTime: 2022-10-20 15:34:42
+ # @FilePath: /EE/Embeded-System/bashPrgramming/example02.bash
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
 #!/bin/bash
 
 echo $0
